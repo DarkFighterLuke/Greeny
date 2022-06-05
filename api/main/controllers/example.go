27af8130c28b48d1a2b243eac9bc5f111c50971d
BrattaFrancesco <fra.bra00@gmail.com>
2022-06-05 15:54:59 +0200

    Created a router example

---
+++ api/main/controllers/example.go
@@ -0,0 +1,17 @@
+package controllers
+
+import "greeny/main/utils"
+
+// getAgentName creates a response for the get-agent-name intent.
+func GetAgentName(request utils.WebhookRequest) (utils.WebhookResponse, error) {
+	response := utils.WebhookResponse{
+		FulfillmentMessages: []utils.Message{
+			{
+				Text: utils.Text{
+					Text: []string{"My name is Greeny"},
+				},
+			},
+		},
+	}
+	return response, nil
+}
