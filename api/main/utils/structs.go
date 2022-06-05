9bf6dc512626fe1992b310b1ce2a07716a7db610
BrattaFrancesco <fra.bra00@gmail.com>
2022-06-05 15:54:19 +0200

    Created structs for webhook request and response

---
+++ api/main/utils/structs.go
@@ -0,0 +1,30 @@
+package utils
+
+type Intent struct {
+	DisplayName string `json:"displayName"`
+	Name        string `json:"name"`
+}
+
+type QueryResult struct {
+	Intent Intent `json:"intent"`
+}
+
+type Text struct {
+	Text []string `json:"text"`
+}
+
+type Message struct {
+	Text Text `json:"text"`
+}
+
+// webhookRequest is used to unmarshal a WebhookRequest JSON object.
+type WebhookRequest struct {
+	Session     string      `json:"session"`
+	ResponseID  string      `json:"responseId"`
+	QueryResult QueryResult `json:"queryResult"`
+}
+
+// webhookResponse is used to marshal a WebhookResponse JSON object.
+type WebhookResponse struct {
+	FulfillmentMessages []Message `json:"fulfillmentMessages"`
+}
