abcbf18a019f87e86585128c84adfb739fb3ea3b
DarkFighterLuke <luca-consoli@live.it>
2022-06-07 20:19:57 +0200

    Fix GetUserFolderPath() returning file nodes

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -7,23 +7,28 @@ import (
 )
 
 func GetUserFolderPath() (string, error) {
-	dirs, err := os.ReadDir("data")
+	nodes, err := os.ReadDir("data")
 	if err != nil {
 		return "", err
 	}
 
-	if len(dirs) < 1 {
+	if len(nodes) < 1 {
 		return "", fmt.Errorf("no directories are present")
 	}
 
-	sort.Slice(dirs, func(i, j int) bool {
-		infoI, err := dirs[i].Info()
+	sort.Slice(nodes, func(i, j int) bool {
+		infoI, err := nodes[i].Info()
 		if err != nil {
 			return false
 		}
-		infoJ, err := dirs[j].Info()
+		infoJ, err := nodes[j].Info()
 		return infoI.ModTime().Before(infoJ.ModTime())
 	})
 
-	return dirs[0].Name(), nil
+	for _, node := range nodes {
+		if node.IsDir() {
+			return node.Name(), nil
+		}
+	}
+	return "", fmt.Errorf("no directories are present")
 }
