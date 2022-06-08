95ef0b28e5e35cd6139c9ddedf5b5a75e16daedc
DarkFighterLuke <luca-consoli@live.it>
2022-06-08 16:25:53 +0200

    Implement reading from italian RTP energy cost file

---
+++ api/utils/energyCost.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	strconv "strconv"
+)
+
+type EnergyCost = [][]float32
+
+func parseEnergyCost(entries [][]string) (EnergyCost, error) {
+	var energyCost EnergyCost
+	for i, dayCosts := range entries {
+		if i == 0 {
+			continue
+		}
+		var dayCostsFloat []float32
+		for _, hourCost := range dayCosts {
+			cost, err := strconv.ParseFloat(hourCost, 32)
+			if err != nil {
+				return nil, err
+			}
+			dayCostsFloat = append(dayCostsFloat, float32(cost))
+		}
+		energyCost = append(energyCost, dayCostsFloat)
+	}
+	return energyCost, nil
+}
+
+func ReadEnergyCost(pathToEnergyCostFile string) (EnergyCost, error) {
+	file, err := os.Open(pathToEnergyCostFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	entries, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	energyCost, err := parseEnergyCost(entries)
+	return energyCost, err
+}
+
+func GetEnergyCostByDayAndHour(energyCost *EnergyCost, dayOfWeek, hour int) (float32, error) {
+	if dayOfWeek < 0 || dayOfWeek > 6 {
+		return 0, fmt.Errorf("day of week out of range")
+	}
+
+	if hour < 0 || hour > 23 {
+		return 0, fmt.Errorf("hour out of range")
+	}
+
+	return (*energyCost)[dayOfWeek+1][hour], nil
+}
