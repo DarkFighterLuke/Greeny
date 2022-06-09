7dad249e903501372e1e5f77227b524ccb7d6ca7
BrattaFrancesco <fra.bra00@gmail.com>
2022-06-09 09:31:58 +0200

    Made power off controller

---
+++ api/controllers/appliancePowerOff.go
@@ -0,0 +1,78 @@
+package controllers
+
+import (
+	"greeny/utils"
+	"os"
+	"time"
+)
+
+func PowerOff(response utils.WebhookRequest) (utils.WebhookResponse, error) {
+	user, err := utils.GetUserFolderPath()
+	if err != nil {
+		return utils.WebhookResponse{}, err
+	}
+	appliance := response.QueryResult.Parameters["appliance"].(string)
+
+	consumptions, err := utils.ReadConsumptions("data/" + user + "/optimal-schedule.csv")
+	if err != nil {
+		return utils.WebhookResponse{}, err
+	}
+
+	consumption, err := utils.FindConsumptionsByApplianceName(&consumptions, appliance)
+	if err != nil {
+		return utils.WebhookResponse{}, err
+	}
+
+	hour := time.Now().Hour()
+	on, err := consumption.IsTurnedOn(hour)
+	if err != nil {
+		return utils.WebhookResponse{}, err
+	}
+
+	if on {
+		shiftable, err := utils.IsApplianceShiftable(user, appliance)
+		if err != nil {
+			return utils.WebhookResponse{}, err
+		}
+		if shiftable {
+			err := utils.MakeApplianceNonShiftable(user, appliance, hour)
+			if err != nil {
+				return utils.WebhookResponse{}, err
+			}
+		}
+
+		err = utils.GenerateOptimalSchedule("data/"+user+"shiftable_temp.csv", "data/"+user+
+			"non-shiftable_temp.csv", "data/"+user+"optimal-schedule.csv")
+		if err != nil {
+			return utils.WebhookResponse{}, err
+		}
+
+		err = os.Remove("data/" + user + "shiftable_temp.csv")
+		if err != nil {
+			return utils.WebhookResponse{}, err
+		}
+		err = os.Remove("data/" + user + "non-shiftable_temp.csv")
+		if err != nil {
+			return utils.WebhookResponse{}, err
+		}
+		return utils.WebhookResponse{
+			FulfillmentMessages: []utils.Message{
+				{
+					Text: utils.Text{
+						Text: []string{"L'elettrodomestico " + appliance + " è stato spento"},
+					},
+				},
+			},
+		}, nil
+	} else {
+		return utils.WebhookResponse{
+			FulfillmentMessages: []utils.Message{
+				{
+					Text: utils.Text{
+						Text: []string{"L'elettrodomestico " + appliance + " è già spento"},
+					},
+				},
+			},
+		}, nil
+	}
+}
